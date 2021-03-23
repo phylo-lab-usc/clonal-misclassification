c17a3cd11e41b6931b6e8bb561905d722cc118a9
Katrina Kaur <kaur@zoology.ubc.ca>
2021-03-23 09:55:27 -0700

    Part 2 file

---
+++ part_2_code.R
@@ -0,0 +1,9 @@
+##Pseudocode for Part 2 of Clonal Misclassification
+##Authors: Katrina Kaur & Rituparna Banerjee
+
+#####Part 2#####
+# Are there phylogenetic methods to assign B-Cell clones to clades, 
+# do they perform better than similarity based metrics?
+
+
+  
