db6868b783086041bc422c3f7133f660ab89e55c
Katrina Kaur <kaur@zoology.ubc.ca>
2021-03-25 10:43:38 -0700

    2 methods to ask about the germline sequences

--- part_1_code.R
+++ part_1_code.R
@@ -8,34 +8,39 @@
 # 1. Download B-cell repertoires from iReceptor / OR simulate data
 # using partis 
 
-# 2. Group B-cell clones into clusters using partis/clonify/BRILIA
+# 2. Group B-cell clones into clusters using partis/clonify
   # these programs are lineage assignment methods
 
-# 3. Build a phylogeny for each cluster
+# 3. Method A - Germline: Using "tips" (clones) to find the germline
+  # Give output of partis/clonify to IMGT for example to find the VDJ regions
+  # Give output of IMGT to RevertToGermline to find the germline sequences
+
+# 4. Build a phylogeny for each cluster
   # the clustering methods may also include tree building
   # other methods include FastTREE, IQTree, RAxML
 
-# 4. Investigate:
+# 5. Investigate: use phylogeny in the following - 
   # diversification rates and if rates change over branches (BEAST)
   # SHM rates (partis/BRILIA ?)
-  # ancestral states / germline sequences (GMYC/Revert to Germline)
-    # to find the ancestral clone use PAUP/Phangorn/Castor/RAxML/IQTree
-    # use both the tips and the ancestral clone to find the germline
-    # ancestor (VDJ genes)
 
-# 5. Reshuffle the nodes at a certain rate (try several rates)
-  # During step 2?
+# 6. Method B - Germline: Using "ancestor" (ancestral clone) to find germline
+    # use the phylogeny to find the ancestral clone using:
+    # PAUP/Phangorn/Castor/RAxML/IQTree
+    # give ancestral clones to IMGT/RevertToGermline to find germline sequences
+
+# 6. Reshuffle the nodes at a certain rate (try several rates)
+  # during step 2 use different rates to reshuffle the clones
   # reshuffle clones into different clusters
   # re-align the reshuffled clonal groups (assuming each cluster shares
   # an ancestor)
 
-# 6. Build a new phylogeny of each cluster
-  # same methods from step 3 and step 4
+# 7. Repeat steps 3-5 on the resuffled clonal groups
 
-# 7. Compare the original trees to reshuffled trees
+# 8. Compare the original trees to reshuffled trees
   # Tree-likeness to compare trees
   # Are the diversification rates, SHM rates, and ancestral states
   # sensitive to the reshuffling method?
+  # Compare Method A and B on the original and reshuffled trees
 
 
 
