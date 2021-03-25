21988cfce05300a08373abd0f64974021c0dccdb
Katrina Kaur <kaur@zoology.ubc.ca>
2021-03-25 09:15:16 -0700

    adding more details to steps 4-5

--- part_1_code.R
+++ part_1_code.R
@@ -5,7 +5,8 @@
 # What is the effect of clonal misclassification on how 
 # sensitive diversification, SHM, and ancestral state reconstruction analyses are?
 
-# 1. Download B-cell repertoires from iReceptor
+# 1. Download B-cell repertoires from iReceptor / OR simulate data
+# using partis 
 
 # 2. Group B-cell clones into clusters using partis/clonify/BRILIA
   # (these programs are lineage assignment methods)
@@ -20,14 +21,18 @@
   # diversification rates and if rates change over branches (BEAST)
   # SHM rates (partis/BRILIA ?)
   # ancestral states / germline sequences (GMYC/Revert to Germline)
+    # to find the ancestral clone use PAUP/Phangorn/Castor/RAxML/IQTree
+    # use both the tips and the ancestral clone to find the germline
+    # ancestor (VDJ genes)
 
 # **5. Reshuffle the nodes at a certain rate (try several rates)
   # During step 2?
-  # After reshuffling clones into different clusters, is alignment
-  # required for each new cluster?
+  # reshuffle clones into different clusters
+  # re-align the reshuffled clonal groups (assuming each cluster shares
+  # an ancestor)
 
 # 6. Build a new phylogeny of each cluster
-  # same methods from step 3
+  # same methods from step 3 and step 4
 
 # 7. Compare the original trees to reshuffled trees
   # Tree-likeness to compare trees
