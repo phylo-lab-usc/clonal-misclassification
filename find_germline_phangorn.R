984a48840ca23b0d2a85b4702bf63d503806b1bc
Katrina Kaur <kaur@zoology.ubc.ca>
2021-07-06 10:11:44 -0700

    use phangorn to find the germline sequence after tree is built

---
+++ find_germline_phangorn.R
@@ -0,0 +1,31 @@
+## After tree building in RAxML, save tree and fasta file
+
+## Load packages
+library(ape)
+library(phangorn)
+library(seqLogo)
+
+#load sequences
+seqs <- read.FASTA(file="sim.fasta")
+
+#change format to phyDat format
+seqs <- as.phyDat(seqs)
+
+#load tree
+#tree is rooted with branch lengths
+tree <- read.tree(file="RAxML_result.sim1.RUN.9")
+
+#resolve polytomies
+tree2 <- multi2di(tree)
+
+#find likelihood of a tree given a model and sequences
+fit <- pml(tree2, seqs, model = "GTR")
+
+#ancestral character reconstruction
+anc.ml <- ancestral.pml(fit, type = "ml")
+
+#plot
+plotAnc(tree2, anc.ml, 17)
+
+#view ancestral sequence
+seqLogo(t(subset(anc.ml, getRoot(tree2), 1:20)[[1]]), ic.scale=FALSE)
