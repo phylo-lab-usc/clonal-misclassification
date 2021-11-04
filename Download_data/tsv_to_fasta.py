df83a00c907afac10567ba906561d1a27b0f9c8c
Katrina Kaur <35740912+kmkaur@users.noreply.github.com>
2021-11-04 11:07:31 -0700

    Update tsv_to_fasta.py
    code to convert tsp to fasta

--- Download_data/tsv_to_fasta.py
+++ Download_data/tsv_to_fasta.py
@@ -1,1 +1,16 @@
+## download dataset from iReceptor
+## extract only the sequence ID and the sequence column of interest
+## for the input file
+
+x= open("/path_to_file.tsv", "r")
+z= x.readlines()
+y= open("/path_to_file.fasta", "w")
+l= len(z)
+for i in range (2, l, 1):
+	f= z[i]
+	l1= len(f)
+	if "\t" in f:
+		n= f.find("\t")
+		y.write(">"+f[0:(n)] + "\n")
+                y.write(f[(n+1):l1] + "\n")
 
