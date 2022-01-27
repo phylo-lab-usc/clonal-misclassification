27f911e324c32c1dd8623cb181cecebdc456b005
Katrina Kaur <35740912+kmkaur@users.noreply.github.com>
2022-01-26 22:38:04 -0800

    Add files via upload
    RAxML for family level trees (slurm job script)

---
+++ tree_building/simulations_build_family_trees.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --account=                # replace this with your own account
+#SBATCH --mem-per-cpu=            # memory; default unit is megabytes
+#SBATCH --time=                   # time (DD-HH:MM)
+#SBATCH --array=1-5000
+#SBATCH --job-name=family_loop
+
+module load StdEnv/2016.4
+module load nixpkgs/16.09
+module load gcc/7.3.0
+module load openmpi/3.1.4
+module load raxml-ng/0.9.0
+
+for i in "$SLURM_ARRAY_TASK_ID"*.fasta
+do
+echo $i
+done
+
+raxml-ng-mpi --model GTR --msa $i --seed random_number --prefix $i --search
