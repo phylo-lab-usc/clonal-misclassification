636c5a6b2667a1aeaeba3e95ba8a80e758392bb1
Katrina Kaur <kaur@zoology.ubc.ca>
2022-02-07 15:55:26 -0800

    add loop

--- simulation_analyses/medusa_analysis.R
+++ simulation_analyses/medusa_analysis.R
@@ -1,12 +1,23 @@
 ## load packages
 library(geiger)
+library(ape)
+library(phytools)
+library(gtools)
+
+temp <- mixedsort(list.files(pattern="*.bestTree"))
+trees <- lapply(temp,read.tree)
+
+## empty lists
+tree2 <- list()
+tree3 <- list()
+m1 <- list()
 
 ## run MEDUSA in geiger R package
-tree <- read.tree(file="*.bestTree")
-tree <- multi2di(tree)
-tree$edge.length <- tree$edge.length + 0.01
-tree <- phytools::force.ultrametric(tree, method = "extend")
-m1 <- medusa(tree)
-print(m1$summary)
-
+for (i in 1:length(trees)){
+tree2[[i]] <- multi2di(trees[[i]])
+tree2[[i]]$edge.length <- tree2[[i]]$edge.length + 0.01
+tree3[[i]] <- phytools::force.ultrametric(tree2[[i]], method = "extend")
+m1[[i]] <- medusa(tree3[[i]]) 
+#print(m1[[i]]$summary)
+}
 
