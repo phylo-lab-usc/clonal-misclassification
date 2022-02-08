940eb7785cc1a3a5c9aeee7133e5a0ee42e66b68
Katrina Kaur <kaur@zoology.ubc.ca>
2022-02-07 16:38:43 -0800

    error if tree is less than 7 tips, exclude small trees

--- simulation_analyses/medusa_analysis.R
+++ simulation_analyses/medusa_analysis.R
@@ -4,20 +4,35 @@ library(ape)
 library(phytools)
 library(gtools)
 
+## load mega trees
 temp <- mixedsort(list.files(pattern="*.bestTree"))
 trees <- lapply(temp,read.tree)
 
-## empty lists
+## exclude trees with less than or equal to 6 tips
+trees_edit <- list()
+
+for (i in 1:length(trees)) {
+  if (length(trees[[i]]$tip.label)>6)
+  {
+    trees_edit[[i]]<-trees[[i]]
+  }
+}  
+
+## new list of trees
+trees_edit <- trees_edit[-which(sapply(trees_edit, is.null))]
+
+
+## run MEDUSA in geiger R package --> tree needs to be at least 7 tips?
 tree2 <- list()
 tree3 <- list()
 m1 <- list()
 
-## run MEDUSA in geiger R package
-for (i in 1:length(trees)){
-tree2[[i]] <- multi2di(trees[[i]])
+for (i in 1:length(trees_edit)){
+tree2[[i]] <- multi2di(trees_edit[[i]])
 tree2[[i]]$edge.length <- tree2[[i]]$edge.length + 0.01
-tree3[[i]] <- phytools::force.ultrametric(tree2[[i]], method = "extend")
+tree3[[i]] <- force.ultrametric(tree2[[i]], method = "extend")
 m1[[i]] <- medusa(tree3[[i]]) 
 #print(m1[[i]]$summary)
 }
 
+saveRDS(m1, file="pd1_mega_medusa.RDS")
