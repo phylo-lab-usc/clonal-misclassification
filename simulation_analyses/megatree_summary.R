f4c6b872d94fa38749726c3e83a7a8b27161597b
Katrina Kaur <kaur@zoology.ubc.ca>
2022-03-02 09:35:03 -0800

    prune megatrees to show monophyletic clades

---
+++ simulation_analyses/megatree_summary.R
@@ -0,0 +1,128 @@
+## investigate family distribution in megatrees
+
+##### load packages #####
+library(geiger)
+library(ape)
+library(phytools)
+library(gtools)
+require("reshape")
+require("dplyr")
+library(ggplot2)
+
+##### load strict functions #####
+## Load old functions
+
+## FUNCTION 1: drop sister tips that share the same trait value
+same_nodes <- function(phylogeny, trait, label_tip){
+  # get tip numbers
+  ntips <- Ntip(phylogeny)
+  
+  # matrix to fill in with parent nodes (node paths)
+  parent_nodes <- matrix(NA_integer_, nrow = ntips, ncol = ntips-1,
+                         dimnames = list(phylogeny$tip.label, 
+                                         seq(ntips+1, (2*ntips)-1, by = 1)))
+  # retrieve paths
+  full_paths <- path_vec(1:ntips, phylogeny$edge, ntips+1)
+  
+  # fill in matrix
+  for(i in 1:ntips){
+    parent_nodes[i, full_paths[[i]]-ntips] = 
+      trait[which(phylogeny$tip.label == label_tip[i])]
+  }
+  
+  # nodes that will be dropped
+  drop_nodes <- apply(parent_nodes, 2, function(x) length(unique(na.exclude(x))) )
+  
+  return(as.numeric(names(which(drop_nodes == 1))))
+}
+
+## FUNCTION 2: retrieve the node numbers for each tip from tip to root (path)
+path_retrieve <- function(tip_number, edge, root_number){
+  
+  #empty list
+  final_path <- c()
+  
+  #get path
+  while(tip_number != root_number){
+    final_path <- c(which(edge[ , 2] == tip_number), final_path)
+    tip_number <- edge[final_path[1], 1]
+  }
+  
+  final_path <- edge[final_path , 1]
+  
+  return(rev(final_path))
+}
+
+# tips as vector
+path_vec <- Vectorize(FUN = path_retrieve, vectorize.args = "tip_number")
+
+
+
+##### analyze mega trees #####
+
+## load trees
+temp <- mixedsort(list.files(pattern="*.bestTree"))
+trees <- lapply(temp,read.tree)
+
+## load data
+data <- list()
+
+for (i in 1:length(trees)) { 
+data[[i]] <- as.data.frame(trees[[i]]$tip.label)
+colnames(data[[i]]) <- "name"
+data[[i]] <- transform(data[[i]], name = colsplit(name, split = "\\_", names = c('fam', 'sim', 'seq')))
+data[[i]] <- do.call(data.frame, data[[i]])
+data[[i]] <- transform(data[[i]], name.fam = colsplit(name.fam, split="\\y", names = c('word', 'trait')))
+data[[i]] <- do.call(data.frame, data[[i]])
+data[[i]]$tip_label <- trees[[i]]$tip.label
+data[[i]] <- data[[i]][,-1]
+data[[i]] <- data[[i]][,-c(2,3)]
+data[[i]] <- data[[i]][,c(2,1)]
+colnames(data[[i]]) <- c("tip_label", "trait")
+}
+
+## run functions
+dtest_ofs_1090 <- list()
+ltest_ofs_1090 <- list()
+rtest_ofs_1090 <- list()
+ptest_ofs_1090 <- list()
+
+for (i in 1:length(trees)) { 
+dtest_ofs_1090[[i]] <- same_nodes(trees[[i]], data[[i]]$trait, data[[i]]$tip_label)
+ltest_ofs_1090[[i]] <- lapply(dtest_ofs_1090[[i]], function(x) extract.clade(trees[[i]], x)$tip.label)
+rtest_ofs_1090[[i]] <- unique(unlist(sapply(ltest_ofs_1090[[i]], '[', -1, simplify = TRUE)))
+ptest_ofs_1090[[i]] <- drop.tip(phy = trees[[i]], tip = rtest_ofs_1090[[i]])
+}
+
+## number of extra tips
+lengths <- list()
+
+for (i in 1:length(trees)) {
+lengths[[i]] <- length(ptest_ofs_1090[[i]]$tip.label)
+}
+
+## number of tips per family 
+data_pruned <- list()
+summary <- list()
+count <- list()
+
+for (i in 1:length(trees)) {
+data_pruned[[i]] <- data[[i]][data[[i]]$tip_label %in% ptest_ofs_1090[[i]]$tip.label,]
+summary[[i]] <- dplyr::count(data_pruned[[i]], trait)
+count[[i]] <- nrow(subset(summary[[i]],n > 1))
+}
+
+##### plotting #####
+#lengths
+length <- as.data.frame(unlist(lengths))
+val <- 5
+
+ggplot(length, aes(x=unlist(lengths))) + geom_histogram(binwidth=1, color="black", fill="grey") + 
+  xlim(0,110) + theme_classic() + xlab("Number of Clades") + ylab("Count") +
+  geom_vline(aes(xintercept = val), colour="blue", linetype="dashed")
+
+#counts
+counts <- as.data.frame(unlist(count))
+
+ggplot(counts, aes(x=unlist(count))) + geom_histogram(binwidth=1, color="black", fill="grey") + 
+  theme_classic() + xlab("Number of Non-Monophyletic Families") + ylab("Count") 
