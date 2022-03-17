ecf03ea199a64badef5f30f9ec94e3483a59e971
Katrina Kaur <kaur@zoology.ubc.ca>
2022-03-16 18:36:10 -0700

    fix colsplit function

--- simulation_analyses/family_distribution_figure.R
+++ simulation_analyses/family_distribution_figure.R
@@ -206,7 +206,7 @@ mixcr_med_all <- rbind(med_1, med_2, med_3, med_4)
 
 ##### load Change-O results #####
 CO_1 <- read.table(file = 'mega_1_db-pass_clone-pass.tsv', sep = '\t', header = TRUE)
-CO_1 <- transform(CO_1, name = colsplit(sequence_id, pattern = "\\_", names = c('fam', 'sim', 'seq')))
+CO_1 <- transform(CO_1, name = colsplit(sequence_id, split = "\\_", names = c('fam', 'sim', 'seq')))
 CO_1 <- do.call(data.frame, CO_1)
 CO_1_list <- split(CO_1,CO_1$name.sim)
 
@@ -226,7 +226,7 @@ for (i in 1:length(CO_1_list)) {
 }
 
 CO_2 <- read.table(file = 'mega_2_db-pass_clone-pass.tsv', sep = '\t', header = TRUE)
-CO_2 <- transform(CO_2, name = colsplit(sequence_id, pattern = "\\_", names = c('fam', 'sim', 'seq')))
+CO_2 <- transform(CO_2, name = colsplit(sequence_id, split = "\\_", names = c('fam', 'sim', 'seq')))
 CO_2 <- do.call(data.frame, CO_2)
 CO_2_list <- split(CO_2,CO_2$name.sim)
 
@@ -246,7 +246,7 @@ for (i in 1:length(CO_2_list)) {
 }
 
 CO_3 <- read.table(file = 'mega_3_db-pass_clone-pass.tsv', sep = '\t', header = TRUE)
-CO_3 <- transform(CO_3, name = colsplit(sequence_id, pattern = "\\_", names = c('fam', 'sim', 'seq')))
+CO_3 <- transform(CO_3, name = colsplit(sequence_id, split = "\\_", names = c('fam', 'sim', 'seq')))
 CO_3 <- do.call(data.frame, CO_3)
 CO_3_list <- split(CO_3,CO_3$name.sim)
 
@@ -266,7 +266,7 @@ for (i in 1:length(CO_3_list)) {
 }
 
 CO_4 <- read.table(file = 'mega_4_db-pass_clone-pass.tsv', sep = '\t', header = TRUE)
-CO_4 <- transform(CO_4, name = colsplit(sequence_id, pattern = "\\_", names = c('fam', 'sim', 'seq')))
+CO_4 <- transform(CO_4, name = colsplit(sequence_id, split = "\\_", names = c('fam', 'sim', 'seq')))
 CO_4 <- do.call(data.frame, CO_4)
 CO_4_list <- split(CO_4,CO_4$name.sim)
 
