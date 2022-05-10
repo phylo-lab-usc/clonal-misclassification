86da1cc50a1c77627a1d2758ec059b5fa159c898
Katrina Kaur <35740912+kmkaur@users.noreply.github.com>
2022-05-10 15:31:02 -0700

    Update change-o.sh
    update options

--- simulation_analyses/change-o.sh
+++ simulation_analyses/change-o.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
-#SBATCH --account=def-mpennell   # replace this with your own account
-#SBATCH --ntasks=1               # number of processes
-#SBATCH --mem-per-cpu=10000       # memory; default unit is megabytes
-#SBATCH --time=00-23:59        # time (DD-HH:MM)
-#SBATCH --mail-user=kaur@zoology.ubc.ca # Send email updates to you or someone else
-#SBATCH --mail-type=ALL          # send an email in all cases (job started, job ended, job aborted)
-#SBATCH --job-name=CHANGE-O
+#SBATCH --account=             # replace this with your own account
+#SBATCH --ntasks=              # number of processes
+#SBATCH --mem-per-cpu=         # memory; default unit is megabytes
+#SBATCH --time=                # time (DD-HH:MM)
+#SBATCH --job-name=            # name
 
 module load python
 source ~/my_venv/bin/activate
