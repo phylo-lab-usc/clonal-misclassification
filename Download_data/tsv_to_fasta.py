6ba0d7c147649158ca5062b107188304288eb4e7
Katalin Voss <ge52lix@mytum.de>
2022-10-23 20:33:32 -0700

    add parsing for input and output

--- Download_data/tsv_to_fasta.py
+++ Download_data/tsv_to_fasta.py
@@ -1,16 +1,27 @@
 ## download dataset from iReceptor
 ## extract only the sequence ID and the sequence column of interest
 ## for the input file
+import argparse
 
-x= open("/path_to_file.tsv", "r")
-z= x.readlines()
-y= open("/path_to_file.fasta", "w")
-l= len(z)
-for i in range (2, l, 1):
-	f= z[i]
-	l1= len(f)
-	if "\t" in f:
-		n= f.find("\t")
-		y.write(">"+f[0:(n)] + "\n")
-                y.write(f[(n+1):l1] + "\n")
-
+parser = argparse.ArgumentParser()
+# Add an argument
+parser.add_argument('-i', '--input_tsv', type=str, required=True)
+parser.add_argument('-o', '--output', type=str, required=True)
+# Parse the argument
+args = parser.parse_args()
+# Print "Hello" + the user input argument
+x = open(args.input_tsv, "r")
+z = x.readlines()
+y = open(args.output, "w")
+l = len(z)
+#TODO: adapt so it can read files like minervina
+for i in range(2, l, 1):
+    f = z[i]
+    l1 = len(f)
+    if "\t" in f:
+        n = f.find("\t")
+        #id = f[0:(n)]
+        s = f[(n + 1):l1].find("\t")
+        #seq = f[(n + 1):(s+1)]
+        y.write(">" + f[0:(n)] + "\n")
+        y.write(f[(n + 1):(s+1)] + "\n")
